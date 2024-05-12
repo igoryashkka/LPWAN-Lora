0b2b183072d8690b2e65a756c6e154521f6db33e
igoryashkka <83956200+igoryashkka@users.noreply.github.com>
2024-05-12 20:43:57 +0300

    Update rec.ino

--- receiver/rec.ino
+++ receiver/rec.ino
@@ -1,222 +1,88 @@
-
-//---------------------------------------- Include Library.
 #include <SPI.h>
 #include <LoRa.h>
-#include <WiFi.h>
-#include <WiFiClientSecure.h>
-#include <UniversalTelegramBot.h>   // Universal Telegram Bot Library written by Brian Lough: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
-#include <ArduinoJson.h>
 
-#define LED_1_Pin   27
-//---------------------------------------- 
+#define SS_PIN 10
+#define RST_PIN 9
+#define DIO0_PIN 2
+#define SNR_EXCELLENT 15
+#define RSSI_EXCELLENT -80
+#define SNR_VERY_GOOD 10
+#define RSSI_VERY_GOOD -90
+#define SNR_GOOD 5
+#define RSSI_GOOD -100
+#define SNR_FAIR 0
+#define RSSI_FAIR -110
 
-//---------------------------------------- LoRa Pin / GPIO configuration.
-#define ss 5
-#define rst 14
-#define dio0 2
+String incomingMessage = "";
+byte localAddress = 0x02;       
+byte destinationMaster = 0x01; 
 
-const char* ssid = "REPLACE_WITH_YOUR_SSID";
-const char* password = "REPLACE_WITH_YOUR_PASSWORD";
-#define BOTtoken "6238120988:AAER26SSJRT_bvE8nXv9A8l4E3DkhQiqrsc"  // your Bot Token (Get from Botfather)
+void setup() {
+  Serial.begin(115200);
+  LoRa.setPins(SS_PIN, RST_PIN, DIO0_PIN);
+  
+  Serial.println("Start LoRa init...");
+  if (!LoRa.begin(433E6)) {             
+    Serial.println("LoRa init failed. Check your connections.");
+    while (true);                       
+  }
+  Serial.println("LoRa init succeeded.");
+}
 
-#define CHAT_ID "308100665"
-
-String Incoming = "";
-
-WiFiClientSecure client;
-UniversalTelegramBot bot(BOTtoken, client);
-
-// Checks for new messages every 1 second.
-int botRequestDelay = 1000;
-unsigned long lastTimeBotRan;
-
-const int ledPin = 2;
-bool ledState = LOW;
-
-// Handle what happens when you receive new messages
-void handleNewMessages(int numNewMessages) {
-  Serial.println("handleNewMessages");
-  Serial.println(String(numNewMessages));
-
-  for (int i=0; i<numNewMessages; i++) {
-    // Chat id of the requester
-    String chat_id = String(bot.messages[i].chat_id);
-    if (chat_id != CHAT_ID){
-      bot.sendMessage(chat_id, "Unauthorized user", "");
-      continue;
-    }
-    
-    // Print the received message
-    String text = bot.messages[i].text;
-    Serial.println(text);
-
-    String from_name = bot.messages[i].from_name;
-
-    if (text == "/start") {
-      String welcome = "Welcome, " + from_name + ".\n";
-      welcome += "Use the following commands to control your outputs.\n\n";
-      welcome += "/led_on to turn GPIO ON \n";
-      welcome += "/led_off to turn GPIO OFF \n";
-      welcome += "/state to request current GPIO state \n";
-      bot.sendMessage(chat_id, welcome, "");
-    }
-
-    if (text == "/led_on") {
-      bot.sendMessage(chat_id, "LED state set to ON", "");
-      ledState = HIGH;
-      digitalWrite(ledPin, ledState);
-    }
-    
-    if (text == "/led_off") {
-      bot.sendMessage(chat_id, "LED state set to OFF", "");
-      ledState = LOW;
-      digitalWrite(ledPin, ledState);
-    }
-    
-    if (text == "/state") {
-      if (digitalRead(ledPin)){
-        bot.sendMessage(chat_id, "LED is ON", "");
-      }
-      else{
-        bot.sendMessage(chat_id, "LED is OFF", "");
-      }
-    }
+void loop() {
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    onReceive(packetSize);
   }
 }
-String Message = "";             
 
-
-byte LocalAddress = 0x02;       //--> address of this device (Slave 1).
-//byte LocalAddress = 0x03;       //--> address of this device (Slave 2).
-
-byte Destination_Master = 0x01; //--> destination to send to Master (ESP32).
-//----------------------------------------
-
-//---------------------------------------- Variable declarations for temperature and humidity values.
-int h = 0;
-float t = 0.0;
-//---------------------------------------- 
-
-//---------------------------------------- Millis / Timer to update temperature and humidity values from DHT11 sensor.
-unsigned long previousMillis_UpdateDHT11 = 0;
-const long interval_UpdateDHT11 = 2000;
-//---------------------------------------- 
-
-//________________________________________________________________________________ Subroutines for sending data (LoRa Ra-02).
-void sendMessage(String Outgoing, byte Destination) {
-  LoRa.beginPacket();             //--> start packet
-  LoRa.write(Destination);        //--> add destination address
-  LoRa.write(LocalAddress);       //--> add sender address
-  LoRa.write(Outgoing.length());  //--> add payload length
-  LoRa.print(Outgoing);           //--> add payload
-  LoRa.endPacket();               //--> finish packet and send it
-}
-//________________________________________________________________________________ 
-
-//________________________________________________________________________________ Subroutines for receiving data (LoRa Ra-02).
 void onReceive(int packetSize) {
-  if (packetSize == 0) return;  //--> if there's no packet, return
-
-  //---------------------------------------- read packet header bytes:
-  int recipient = LoRa.read();        //--> recipient address
-  byte sender = LoRa.read();          //--> sender address
-  byte incomingLength = LoRa.read();  //--> incoming msg length
-  //---------------------------------------- 
-
-  // Clears Incoming variable data.
-  Incoming = "";
-
-  //---------------------------------------- Get all incoming data.
+  int recipient = LoRa.read();       
+  byte sender = LoRa.read();         
+  byte incomingLength = LoRa.read();  
+  incomingMessage = "";
+  
   while (LoRa.available()) {
-    Incoming += (char)LoRa.read();
+    incomingMessage += (char)LoRa.read();
   }
-  //---------------------------------------- 
 
-  //---------------------------------------- Check length for error.
-  if (incomingLength != Incoming.length()) {
-    Serial.println("error: message length does not match length");
-    return; //--> skip rest of function
+  if (incomingLength != incomingMessage.length()) {
+    Serial.println("Error: Message length does not match length.");
+    return; 
   }
-  //---------------------------------------- 
 
-  //---------------------------------------- Checks whether the incoming data or message for this device.
-  if (recipient != LocalAddress) {
-    Serial.println("This message is not for me.");
-    return; //--> skip rest of function
+  adjustSettingsBySignalQuality();
+  printReceivedMessage(sender, incomingLength);
+}
+
+void adjustSettingsBySignalQuality() {
+  float currentSNR = LoRa.packetSnr();
+  int currentRSSI = LoRa.packetRssi();
+
+  if (currentSNR > SNR_EXCELLENT && currentRSSI > RSSI_EXCELLENT) {
+    adjustLoraADR(7, 14, 500E3, 5);
+  } else if (currentSNR > SNR_VERY_GOOD && currentRSSI > RSSI_VERY_GOOD) {
+    adjustLoraADR(8, 17, 250E3, 6);
+  } else if (currentSNR > SNR_GOOD && currentRSSI > RSSI_GOOD) {
+    adjustLoraADR(9, 18, 125E3, 7);
+  } else if (currentSNR > SNR_FAIR && currentRSSI > RSSI_FAIR) {
+    adjustLoraADR(10, 20, 125E3, 8);
+  } else {
+    adjustLoraADR(12, 20, 125E3, 8);
   }
-  //---------------------------------------- 
+}
 
-  //---------------------------------------- if message is for this device, or broadcast, print details:
+void adjustLoraADR(int sf, int txPower, long sbw, int codingRate) {
+  LoRa.setSpreadingFactor(sf);
+  LoRa.setTxPower(txPower);
+  LoRa.setSignalBandwidth(sbw);
+  LoRa.setCodingRate4(codingRate);
+}
+
+void printReceivedMessage(byte sender, byte incomingLength) {
   Serial.println();
   Serial.println("Received from: 0x" + String(sender, HEX));
   Serial.println("Message length: " + String(incomingLength));
-  Serial.println("Message: " + Incoming);
-  //Serial.println("RSSI: " + String(LoRa.packetRssi()));
-  //Serial.println("Snr: " + String(LoRa.packetSnr()));
-  //---------------------------------------- 
-
-  // Calls the Processing_incoming_data() subroutine.
-//  Processing_incoming_data();
-}
-//________________________________________________________________________________ 
-
-//________________________________________________________________________________ Subroutines to process data from incoming messages, then send messages to the Master.
-
-//________________________________________________________________________________ VOID SETUP
-void setup() {
-  // put your setup code here, to run once:
-
-  Serial.begin(115200);
-
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, ledState);
-  
-  // Connect to Wi-Fi
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  
-    client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
-  }
-  // Print ESP32 Local IP Address
-  Serial.println(WiFi.localIP());
-  // Route for root / web page
-
-
-  //---------------------------------------- Settings and start Lora Ra-02.
-  LoRa.setPins(ss, rst, dio0);
-  
+  Serial.println("Message: " + incomingMessage);
   Serial.println();
-  Serial.println("Start LoRa init...");
-  if (!LoRa.begin(433E6)) {             // initialize ratio at 915 or 433 MHz
-    Serial.println("LoRa init failed. Check your connections.");
-    while (true);                       // if failed, do nothing
-  }
-  Serial.println("LoRa init succeeded.");
-    bool status; 
-
-  
-  //---------------------------------------- 
-}
-//________________________________________________________________________________ 
-
-//________________________________________________________________________________ VOID LOOP
-void loop() {
-  if (millis() > lastTimeBotRan + botRequestDelay)  {
-    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
-     onReceive(LoRa.parsePacket());
-    while(numNewMessages) {
-      Serial.println("got response");
-      handleNewMessages(numNewMessages);
-      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
-    }
-    lastTimeBotRan = millis();
-
-  //---------------------------------------- parse for a packet, and call onReceive with the result:
- 
-  //----------------------------------------
-}
 }
